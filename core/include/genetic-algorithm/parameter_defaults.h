dfbaec0937fab18bd36442a8d763d2db1f3d2712
infopek <10saero01@gmail.com>
2024-05-01 15:43:40 +0200

    Remove tests

--- core/include/genetic-algorithm/parameter_defaults.h
+++ core/include/genetic-algorithm/parameter_defaults.h
@@ -2,9 +2,9 @@
 
 namespace core
 {
-    inline constexpr size_t populationSizeDefault = 400;
+    inline constexpr size_t populationSizeDefault = 300;
     inline constexpr size_t parentSelectionSizeDefault = 20;
-    inline constexpr size_t matingPoolSizeDefault = 40;
+    inline constexpr size_t matingPoolSizeDefault = 20;
     inline constexpr float crossoverRateDefault = 0.78f;
     inline constexpr float mutationRateDefault = 0.6f;
 }
